f312276884630b471c82f6054855c01575d23b59
unknown <anastasiyakov99@gmail.com>
2023-11-08 14:21:29 +0200

    Finished animation on current track title.

--- js/player.js
+++ js/player.js
@@ -279,7 +279,7 @@ const activatePlaylist = (playlistId, playerId) => {
         });
 
         // add animation if link isn't fully visible:
-        if (link.offsetWidth >= link.parentNode.clientWidth) {
+        if (link.offsetWidth >= link.parentNode.offsetWidth) {
             // add class 'animated' to link:
             link.classList.add('animated');
             // add animation-duration:
